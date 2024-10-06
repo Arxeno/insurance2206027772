97dbcda18c2b4fa715877bf5fabbe4819ea38edd
Arxeno <rmfbkw04@gmail.com>
2024-10-06 19:25:38 +0700

    Initial commit

---
+++ src/main/java/apap/ti/insurance2206027772/Insurance2206027772Application.java
@@ -0,0 +1,13 @@
+package apap.ti.insurance2206027772;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Insurance2206027772Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Insurance2206027772Application.class, args);
+	}
+
+}
---
+++ src/test/java/apap/ti/insurance2206027772/Insurance2206027772ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti.insurance2206027772;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Insurance2206027772ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
