c389128288a165d141e731560f9b5bc4aedd5aae
Arxeno <rmfbkw04@gmail.com>
2024-10-17 23:35:37 +0700

    create template controllers

---
+++ src/main/java/apap/ti/insurance2206027772/controllers/PolicyController.java
@@ -0,0 +1,8 @@
+package apap.ti.insurance2206027772.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/policy")
+public class PolicyController {}
