5a7bc1e0c61b1eb553c504b7b46e1c080be6e2c3
Arxeno <rmfbkw04@gmail.com>
2024-10-17 22:52:11 +0700

    fix(db): set deleted at

--- src/main/java/apap/ti/insurance2206027772/models/BaseDeletedAt.java
+++ src/main/java/apap/ti/insurance2206027772/models/BaseDeletedAt.java
@@ -2,8 +2,12 @@ package apap.ti.insurance2206027772.models;
 
 import jakarta.persistence.MappedSuperclass;
 import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 
 @MappedSuperclass
+@Setter
+@Getter
 public class BaseDeletedAt extends Base {
 
   private Date deletedAt;
