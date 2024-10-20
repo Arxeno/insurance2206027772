b9fcd282a2f8b208ef8d8e9cacbc1025e19ff7d1
Arxeno <rmfbkw04@gmail.com>
2024-10-20 13:57:12 +0700

    fix(add company): size validation

--- src/main/java/apap/ti/insurance2206027772/dtos/request/AddCompanyRequestDTO.java
+++ src/main/java/apap/ti/insurance2206027772/dtos/request/AddCompanyRequestDTO.java
@@ -3,6 +3,8 @@ package apap.ti.insurance2206027772.dtos.request;
 import apap.ti.insurance2206027772.models.Coverage;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 import lombok.Getter;
@@ -13,20 +15,21 @@ import lombok.Setter;
 @Data
 public class AddCompanyRequestDTO {
 
-  @NotBlank
+  @NotBlank(message = "Nama wajib diisi.")
   private String name;
 
-  @NotBlank
+  @NotBlank(message = "Kontak wajib diisi.")
   private String contact;
 
-  @NotBlank
-  @Email
+  @NotBlank(message = "Email wajib diisi.")
+  @Email(message = "Format email tidak valid.")
   private String email;
 
-  @NotBlank
+  @NotBlank(message = "Alamat wajib diisi.")
   private String address;
 
-  private List<Coverage> listCoverage;
+  @Size(min = 1, message = "Minimal memiliki 1 coverage.")
+  private List<Coverage> listCoverage = new ArrayList<>();
 
   public Long getTotalCoverage(List<Coverage> allCoveragesFromDb) {
     Long totalCoverage = 0L;
